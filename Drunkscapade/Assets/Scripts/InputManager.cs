c4acd7be03fed5d1fe6a5473984226b2690438c1
Grisel Alma <griselalmasanchez@gmail.com>
2020-10-10 12:51:44 -0300

    Generate random key codes

--- Drunkscapade/Assets/Scripts/InputManager.cs
+++ Drunkscapade/Assets/Scripts/InputManager.cs
@@ -1,19 +1,25 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour
 {
     public static InputManager Instance { get; private set; }
 
-    [SerializeField] private KeyCode _moveForward;
-    
-    private UnityEvent _onPressMoveForward = new UnityEvent();
-    private UnityEvent _onReleaseMoveForward = new UnityEvent();
+    private static KeyCode _moveForward;
+    private static KeyCode _moveBackward;
+    private static KeyCode _moveRight;
+    private static KeyCode _moveLeft;
 
-    public UnityEvent OnPressMoveForward => _onPressMoveForward;
+    [SerializeField] private KeyCode[] _movementKeys = { _moveForward, _moveBackward, _moveRight, _moveLeft };
+
+    private KeyCode[] _keyCodes = { KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D };
+
+    private List<int> _pickedKeys = new List<int>();
+
+    public bool IsMovingForward { get; private set; }
+    public bool IsMovingBackward { get; private set; }
+    public bool IsMovingRight { get; private set; }
+    public bool IsMovingLeft { get; private set; }
 
     private void Awake()
     {
@@ -25,18 +31,45 @@ public class InputManager : MonoBehaviour
         {
             Instance = this;
         }
+        PopulatePickedKeys();
     }
 
     private void Update()
     {
-        if(Input.GetKeyDown(_moveForward))
+        if (Input.GetKeyDown(KeyCode.G))
         {
-            _onPressMoveForward.Invoke();
+            GenerateRandomKeyCodes();
         }
 
-        if (Input.GetKeyUp(_moveForward))
+        IsMovingForward = Input.GetKey(_moveForward);
+        IsMovingBackward = Input.GetKey(_moveBackward);
+        IsMovingRight = Input.GetKey(_moveRight);
+        IsMovingLeft = Input.GetKey(_moveLeft);
+    }
+
+    private void PopulatePickedKeys()
+    {
+        for (int n = 0; n < _keyCodes.Length; n++)
         {
-            _onReleaseMoveForward.Invoke();
+            _pickedKeys.Add(n);
         }
+    }
+
+    private KeyCode GenerateRandomKeyCode()
+    {
+        int randomKey = UnityEngine.Random.Range(0, _pickedKeys.Count - 1);
+        KeyCode movementKey = _keyCodes[_pickedKeys[randomKey]];
+        _pickedKeys.RemoveAt(randomKey);
+        return movementKey;
+    }
+
+    private void GenerateRandomKeyCodes()
+    {
+        for (int n = 0; n < _movementKeys.Length; n++)
+        {
+            _movementKeys[n] = GenerateRandomKeyCode();
+        }
+
+        PopulatePickedKeys();
     }
 }
