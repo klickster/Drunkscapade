556f40c4deb9595dfffc35fa3f7116532122058b
luchopb <luchopb.2@gmail.com>
2020-10-10 11:08:04 -0300

    InputManager Singleton
    Materials Folder

---
+++ Drunkscapade/Assets/Scripts/InputManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+}
