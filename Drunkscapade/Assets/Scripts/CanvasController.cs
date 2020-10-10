36136057e8ce22c45c01b4e6908fdac1310ba3d2
luchopb <luchopb.2@gmail.com>
2020-10-10 13:36:49 -0300

    Sleep Quick Time Event

---
+++ Drunkscapade/Assets/Scripts/CanvasController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasController : MonoBehaviour
+{
+    [SerializeField] private Image _wakeUpBar;
+
+    public void UpdateWakeUpBar(float value)
+    {
+        _wakeUpBar.fillAmount = value;
+    }
+}
